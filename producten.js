cdcc32ca4f23ae51b86d632e9b7fc8d3adb15885
SizzlinZebra <giulianodoorten08@gmail.com>
2026-02-12 13:10:09 +0100

    javascript array
    basis voor planten layout

---
+++ producten.js
@@ -0,0 +1,11 @@
+const planten = [
+  {
+    id: 1,
+    naam: "plantje",
+    prijs: 420.67,
+    categorie: "Binnenplanten",
+    afbeelding: "images/plantjes.jpg",
+    beschrijving: "Plant."
+  },
+  
+];
